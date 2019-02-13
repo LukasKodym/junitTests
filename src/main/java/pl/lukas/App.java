7df1da321a0a17a29302eb1edf8591d3bb260c66
LukasKodym <lukasz.kodym@gmail.com>
2019-02-13 17:40:54 +0100

    Initial commit

---
+++ src/main/java/pl/lukas/App.java
@@ -0,0 +1,13 @@
+package pl.lukas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
