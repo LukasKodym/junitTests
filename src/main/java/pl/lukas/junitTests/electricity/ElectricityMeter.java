003912381c4af85b666d4a060dbfc88323e03d09
LukasKodym <lukasz.kodym@gmail.com>
2019-02-13 19:01:38 +0100

    electricity class update

--- src/main/java/pl/lukas/junitTests/electricity/ElectricityMeter.java
+++ src/main/java/pl/lukas/junitTests/electricity/ElectricityMeter.java
@@ -1,14 +1,57 @@
 package pl.lukas.junitTests.electricity;
 
+import java.util.Calendar;
+
 public class ElectricityMeter {
 
     private float kwh = 0;
     private int centsForKwh = 0;
+
     private boolean tariffOn = false;
     private float kwhTariff = 0;
     private int centsForKwhTariff = 0;
+
     private int electricityTariffStartHour = 0;
-    private int electricityTariffEndtHour = 0;
+    private int electricityTariffEndHour = 0;
 
+    public void addKwh(float kwhToAdd) {
+        if (isTariffNow()) {
+            kwhTariff += kwhToAdd;
+        } else {
+            kwh += kwhToAdd;
+        }
+    }
 
+    private boolean isTariffNow() {
+        Calendar rightNow = Calendar.getInstance(); // method getInstance is taking actual date from calendar
+        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+        return hour > electricityTariffStartHour && hour < electricityTariffEndHour;
+    }
+
+    /**
+     * @return how much more expensive is normal price to tariff in percentages
+     */
+    public int getHowMoreExpensiveNormalIs() {
+        return (centsForKwh * 100 / centsForKwhTariff) - 100;
+    }
+
+    void setCentsForKwh(int centsForKwh) {
+        this.centsForKwh = centsForKwh;
+    }
+
+    void setKwhTariff(float kwhTariff) {
+        this.kwhTariff = kwhTariff;
+    }
+
+    void setCentsForKwhTariff(int centsForKwhTariff) {
+        this.centsForKwhTariff = centsForKwhTariff;
+    }
+
+    void setElectricityTariffStartHour(int electricityTariffStartHour) {
+        this.electricityTariffStartHour = electricityTariffStartHour;
+    }
+
+    void setElectricityTariffEndHour(int electricityTariffEndHour) {
+        this.electricityTariffEndHour = electricityTariffEndHour;
+    }
 }
