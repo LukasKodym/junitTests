76be5f9cb3e9cc0aa9f8f729d2e65b21bd1c81a6
LukasKodym <lukasz.kodym@gmail.com>
2019-02-13 18:21:58 +0100

    electricity class

---
+++ src/main/java/pl/lukas/junitTests/electricity/ElectricityMeter.java
@@ -0,0 +1,14 @@
+package pl.lukas.junitTests.electricity;
+
+public class ElectricityMeter {
+
+    private float kwh = 0;
+    private int centsForKwh = 0;
+    private boolean tariffOn = false;
+    private float kwhTariff = 0;
+    private int centsForKwhTariff = 0;
+    private int electricityTariffStartHour = 0;
+    private int electricityTariffEndtHour = 0;
+
+
+}
