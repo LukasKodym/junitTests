d3db3adb7e78aeda5b3113abc8373242f97001c6
LukasKodym <lukasz.kodym@gmail.com>
2019-02-14 17:16:03 +0100

    new tests, examples of naming tests, GivenWhenThen principle

--- src/test/java/pl/lukas/junitTests/electricity/ElectricityMeterTest.java
+++ src/test/java/pl/lukas/junitTests/electricity/ElectricityMeterTest.java
@@ -3,16 +3,52 @@ package pl.lukas.junitTests.electricity;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ElectricityMeterTest {
 
     @Test
-    public void addKwh() {
-
+    public void addKwh_newMeter_properAddition() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
         electricityMeter.addKwh(1);
         Assert.assertTrue(electricityMeter.getKwh() == 1);
+    }
 
+    @Test
+    public void addKwh_newMeter2_properAddition() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(4);
+        Assert.assertTrue(electricityMeter.getKwh() == 5);
+    }
+
+    @Test
+    public void addKwh_newMeter5_properAddition() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(4);
+        electricityMeter.addKwh(4);
+        Assert.assertTrue(electricityMeter.getKwh() == 17);
+    }
+
+    @Test
+    public void kwhCounterIncreaseIfNew() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 1);
+    }
+
+    @Test
+    public void kwhCounterIncreaseIfSecond() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 2);
+    }
+    @Test
+    public void givenNewMeterWhenFirsAdditionThenProperCounter() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 1);
     }
 }
