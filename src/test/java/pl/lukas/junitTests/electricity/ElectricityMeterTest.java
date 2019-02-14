2020b0c739240b2e79b6e2d6a566795ead3d7c24
LukasKodym <lukasz.kodym@gmail.com>
2019-02-14 18:15:45 +0100

    GWT principal in use (splitting fields in code), not only in naming of methods

--- src/test/java/pl/lukas/junitTests/electricity/ElectricityMeterTest.java
+++ src/test/java/pl/lukas/junitTests/electricity/ElectricityMeterTest.java
@@ -50,9 +50,13 @@ public class ElectricityMeterTest {
 
     @Test
     public void givenNewMeterWhenFirsAdditionThenProperCounter() {
+        // given
         ElectricityMeter electricityMeter = new ElectricityMeter();
+        // when // we can use labels e.g. When:, but cannot be use in declare of a new variable
         electricityMeter.addKwh(1);
+        // then
         Assert.assertTrue(electricityMeter.getKwh() == 1);
+        // given, when, then = arrange, act, assert; bette use G-W-T signature
     }
 
     @Test(expected = ArithmeticException.class)
